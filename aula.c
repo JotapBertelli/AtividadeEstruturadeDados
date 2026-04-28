ce6eca0c6b17874084f8c5af653a7b78367a2982
João Pedro da Silva Bertelli <109303186+JotapBertelli@users.noreply.github.com>
2026-04-27 22:21:40 -0300

    Add files via upload

--- aula.c
+++ aula.c
@@ -1,49 +1,25 @@
+// lista lineares - Estrutura de Dados Aula 27/04
+
 #include <stdio.h>
-#include <string.h>
-typedef struct {
-	char nome[30];
-	int idade;
-	float salario;
-}Pessoa ;
 
 
-/*
-Pessoa criaPessoa(){
-		Pessoa johnBrober ; 
-		printf("\n Informe o nome:  ");
-		scanf("%s",johnBrober.nome);
-		printf("\n Informe a idade:  ");
-		scanf("%d", johnBrober.idade);
-		printf("Informe o salário: ");
-		scanf("%f", johnBrober.salario);
-		return johnBrober;
-}
-*/
+//push(p1,'m')
+//x = pop(p1)
+//push(p1,x)
 
 
+typedef struct{
+	char titulo[35];
+	char autor[35];
+}Livro;
 
-void apresentaPessoa(Pessoa p){
-	printf("\n---------------------------------------------------------------");
-	printf("\n|-%30s   %5d    R$ %6.2f|",p.nome,p.idade,p.salario);
-	printf("\n---------------------------------------------------------------\n\n");
-}
+typedef struct{
+	int topo;
+	Livro lv[MAX]
+}Pilha;
 
-void dobrarSalario(Pessoa *  p){  //recebe o endereço de uma pessoa
-	p->salario = 2 * p->salario;
-	//(*p).salario = 2 * (*p).salario; //tambem funciona !!!
-}
-
-void main(){
-	Pessoa maria = {"Maria Alice",19,7600.00};
-	Pessoa pedro,joao ; 
-	strcpy(pedro.nome, "Pedro Lucas");
-	pedro.idade = 30;
-	pedro.salario = 4500.00;
-	
-	
-	apresentaPessoa(pedro) ;
-	apresentaPessoa(maria) ;
-	joao = pedro;
-	apresentaPessoa(joao) ; //Passa o endereço do João
-	dobraSalario(&joao);
-}
+void initPilha(Pilha * p)
+int pilhaCheia(Pilha p)
+int pilhaVazia(Pilha p)
+int push(Livro L, Pilha * p)
+Livro * pop(Pilha * p)
