58a453293368713d08496d0437427081a5f7d122
João Pedro da Silva Bertelli <109303186+JotapBertelli@users.noreply.github.com>
2026-05-11 21:03:18 -0300

    Fix struct definition and add initialization function

--- Filas.c
+++ Filas.c
@@ -1,51 +1,84 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define MAX 50
 
-
-typedef strcut{
+typedef struct{
 	char titulo[40];
 	char interprete[40];
 	char caminho[60];
 }Musica;
 
-
-
 typedef struct{
-	musica vetmus[MAX];
+	Musica vetmus[MAX];
 	int inicial;
 	int final;
 }FilaMus;
 
-int FilaVazia(FilaMus F){
+int FilaVazia(FilaMus f){
 	return(f.inicial == f.final);
-	
-}
-int FilaCheia(FilaMus F){
-	return(f.final > MAX - 1 );
 }
 
-int inserir(FilaMus *f,Musica m){
+int FilaCheia(FilaMus f){
+	return(f.final > MAX - 1);
+}
+
+void initFilaMus(FilaMus *f){
+	f->inicial = 0;
+	f->final = 0;
+}
+
+int inserir(FilaMus *f, Musica m){
 	if(FilaCheia(*f))
-	return -1;
+		return -1;
 	else{
-		f ->vetmus[f->final] = m;
-		f->final = f->final++;
+		f->vetmus[f->final] = m;
+		f->final = f->final + 1;
 		return 0;
 	}
 }
 
-/// função de retirar
+Musica* retirar(FilaMus *f){
+	if(FilaVazia(*f))
+		return NULL;
+	else{
+		Musica *pm = (Musica*) malloc(sizeof(Musica));
+		if(pm != NULL){
+			*pm = f->vetmus[f->inicial];
+			f->inicial = f->inicial + 1;
+		}
+		return pm;
+	}
+}
 
+void playMus(Musica m){
+	char comando[150];
+	sprintf(comando, "start \"\" \"%s\"", m.caminho);
+	system(comando);
+}
 
-void menu(){
-	printf("\n---- Menu Musical -----\n")
-	printf("")
+void mostrarMusica(Musica m){
+	printf("\nTÃ­tulo: %s\nIntÃ©rprete: %s\nCaminho: %s\n", m.titulo, m.interprete, m.caminho);
 }
 
 
 
-
-
-
-//final: onde vai ser armazenada a próxima música (se fila não cheia) inicio = 0;
-//inicial : onde está próxima música a ser retirada (se fila não vazia) inicio = 0;
+int main(){
+	Musica m1 = {"Mina do Condominio", "Seu Jorge", "C:\\Users\\Aluno\\Music\\minadocondominio.mp3"};
+	FilaMus playlist;
+	int inseriu_OK;
+	
+	initFilaMus(&playlist);
+	inseriu_OK = inserir(&playlist, m1);
+	
+	if(inseriu_OK == 0){
+		printf("Musica inserida com sucesso!\n");
+		mostrarMusica(m1);
+		printf("\nTocando mÃºsica...\n");
+		playMus(m1);
+	}
+	else{
+		printf("Erro ao inserir musica!\n");
+	}
+	
+	return 0;
+}
